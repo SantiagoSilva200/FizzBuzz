f6f7644b43c9a1a25fa7b9230e3cc0a54f75c08d
SantiagoSilva200 <santiago10y66@outlook.es>
2026-02-27 22:26:40 +0000

    Inicio TDD

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,19 @@
+public class FizzBuzz {
+
+    public static String fizzbuzz(int n) {
+
+        if (n % 3 == 0 && n % 5 == 0) {
+            return "FizzBuzz";
+        }
+
+        if (n % 3 == 0) {
+            return "Fizz";
+        }
+
+        if (n % 5 == 0) {
+            return "Buzz";
+        }
+
+        return String.valueOf(n);
+    }
+}
